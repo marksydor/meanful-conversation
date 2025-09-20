0b3f29109ed8a6f458cf3b518d6ab470b552c244
Mark Sydor <msydor@its.jnj.com>
2025-09-20 01:59:27 -0400

    added tool suport

--- src/classes/base_llm_connector.py
+++ src/classes/base_llm_connector.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 import types
+from typing import List
+from urllib import response
+
+from models.llm_tool import LLMTool
 
 class BaseLLMConnector(ABC):
     def __init__(self, model, system_prompt):
@@ -16,6 +20,8 @@ class BaseLLMConnector(ABC):
         self._is_stream_in_process = False
         self._stream = None
         self._is_printing_enabled = False
+        self.is_tools_available = False
+        self.tools: List[LLMTool] = []
         
     def reset(self):
         self._question = None
@@ -26,7 +32,25 @@ class BaseLLMConnector(ABC):
         self._is_printing_enabled = False
         self.on_stream_event = None
         self._messages = []
-    
+
+    def _prepare_tools(self):
+        return [tool.to_json() for tool in self.tools]
+
+    def add_tool(self, tool: LLMTool):
+        self.tools.append(tool)
+        self.is_tools_available = True
+        return self
+
+    def remove_tool(self, tool_name: str):
+        self.tools = [tool for tool in self.tools if tool.name != tool_name]
+        self.is_tools_available = bool(self.tools)
+        return self
+
+    def set_tools(self, tools: List[LLMTool]):
+        self.tools = tools
+        self.is_tools_available = bool(tools)
+        return self
+
     def ask(self, question: str):
         self._is_single_ask_mode_enabled = True
         self._is_conversation_mode_enabled = False
@@ -93,7 +117,11 @@ class BaseLLMConnector(ABC):
 
     def _stream_wrapper(self, stream):
         for event in stream:
+            if self.is_stream_tool_run_needed(event) and self.is_tools_available:
+                return self._run_tools(event)
+            
             chunk = self._parse_stream_chunk(event)
+            
             if chunk:
                 yield chunk
             elif chunk in {"message.error", "message.stopped", "error"}:
@@ -106,7 +134,7 @@ class BaseLLMConnector(ABC):
         
         try:
             for chunk in self._stream_wrapper(stream):
-                
+
                 cleaned = chunk.replace("```", "")
                 parts.append(cleaned)
                 
@@ -114,8 +142,8 @@ class BaseLLMConnector(ABC):
                 
                 if self._is_printing_enabled:
                     print(cleaned, end='', flush=True)
-                    
-                if self.on_stream_event and isinstance(self.on_stream_event, types.FunctionType):
+
+                if self.on_stream_event and callable(self.on_stream_event):
                     self.on_stream_event(chunk)
                     
             return self.last_result
@@ -124,9 +152,52 @@ class BaseLLMConnector(ABC):
         finally:
             self._is_stream_in_process = False
         
+    @abstractmethod
+    def is_tool_run_needed(self, response) -> bool:
+        pass
+    
+    @abstractmethod
+    def is_stream_tool_run_needed(self, response) -> bool:
+        pass
+
+    @abstractmethod
+    def _get_tool_name(self, response) -> str:
+        pass
+    
+    @abstractmethod
+    def _get_tool_args(self, response, tool_name: str) -> dict:
+        pass
+
+    @abstractmethod
+    def _return_tool_results_back_to_llm(self, tool_results) -> dict:
+        pass
+
+    @abstractmethod
+    def _get_tool_names_list(self, response) -> List[str]:
+        pass
+
+    def _run_tools(self, response):
+        if not self.is_tools_available:
+            raise RuntimeError("No tools available to run.")
+        
+        tool_results = []
+        
+        for tool_name in self._get_tool_names_list(response):
+            tool = next((t for t in self.tools if t.name == tool_name), None)
+            if not tool:
+                raise ValueError(f"Tool '{tool_name}' not found.")
+            args = self._get_tool_args(response, tool_name)
+            tool_result = tool.execute(**args)
+            tool_results.append({"name": tool_name, "result": tool_result})
+
+        return self._return_tool_results_back_to_llm(tool_results)
+
     def _run_default(self):
         response = self._get_connector_response()
-        
+
+        if self.is_tool_run_needed(response) and self.is_tools_available:
+           return self._run_tools(response)
+            
         parsed_response = self._parse_response(response)
 
         self.last_result = parsed_response
